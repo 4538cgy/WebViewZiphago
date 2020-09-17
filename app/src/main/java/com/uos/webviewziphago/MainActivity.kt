e003e9b9311331a98b5c3ce11534a0b1417aaa0c
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-09-17 22:22:25 +0900

    final commit

--- app/src/main/java/com/uos/webviewziphago/MainActivity.kt
+++ app/src/main/java/com/uos/webviewziphago/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.uos.webviewziphago
 
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.LayoutInflater
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var backKeyPressedTime = 0
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -19,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val webView = findViewById<View>(R.id.webview) as WebView
+         val webView = findViewById<View>(R.id.webview) as WebView
 
         WebView.setWebContentsDebuggingEnabled(true)
 
@@ -36,13 +43,50 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
 
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()){
+
+    override fun onBackPressed() {
+
+
+
+
+
+        var curTime = System.currentTimeMillis()
+        var gapTime = curTime - backKeyPressedTime
+
+        if(webview.canGoBack()){
             webview.goBack()
-        return true
+        }else if(0<=gapTime && 2000 >= gapTime){
+            super.onBackPressed()
+
+        }else{
+            backKeyPressedTime = curTime.toInt()
+            showSettingPopup()
+
+        }
+
+
+
+
     }
 
-        return super.onKeyDown(keyCode, event)
+    fun showSettingPopup(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_popup,null)
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("앱을 종료하시겠습니까?")
+            .setNeutralButton("아니오",null)
+            .setPositiveButton("네") {
+                dialog , which ->
+                finishAffinity()
+            }
+
+            .create()
+
+
+        alertDialog.setView(view)
+        alertDialog.show()
     }
+
+
 }
